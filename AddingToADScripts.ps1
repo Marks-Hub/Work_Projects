5cc85bc895776251c1986b851077c16b1698d259
Marks-Hub <118840662+Marks-Hub@users.noreply.github.com>
2025-02-17 12:39:14 -0500

    Add files via upload

---
+++ AddingToADScripts.ps1
@@ -0,0 +1,19 @@
+﻿Import-CSV "C:\Users\mokin\OneDrive\Documents\AD employees\Extra People AD.csv" | ForEach-Object {
+    $firstName = $_.FirstName
+    $lastName = $_.LastName
+    $fullName = "$firstName $lastName"
+    $userName = $_.UserName
+    $userPrincipalName = "$userName@rushordertees.local"
+    
+    New-ADUser -GivenName $firstName `
+               -Surname $lastName `
+               -Name $fullName `
+               -UserPrincipalName $userPrincipalName `
+               -SamAccountName $userName `
+               -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -Force) `
+               -Enabled $true `
+               -Path "OU=Domain Users,OU=Accounts,OU=Commerce,OU=ROT,DC=rushordertees,DC=local" `
+               -PasswordNeverExpires $false `
+               -ChangePasswordAtLogon $true `
+               -PassThru
+}
